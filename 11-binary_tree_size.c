bfe086f73538701303f8197cb9fd4d6fa99dd9ef
Wambani01 <georgewmbn@gmail.com>
2023-03-01 12:24:50 -0800

    counting tree leaves

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left = 0;
 	size_t right = 0;
-	
+
 	if (tree == NULL)
-		return 0;
+		return (0);
 	left = binary_tree_size(tree->left);
 	right = binary_tree_size(tree->right);
 	return (left + right + 1);
